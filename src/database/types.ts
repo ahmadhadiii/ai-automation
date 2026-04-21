80af673a588db7a86d8a90d6bd5bfc7776b3c9cc
koki <ahmed.hadi.adolfsberg@gmail.com>
2026-04-21 16:47:05 +0300

    feat: story-1 Database Foundation

--- src/database/types.ts
+++ src/database/types.ts
@@ -27,7 +27,7 @@ export interface UsersTable {
   password_hash: string;
   first_name: string;
   last_name: string;
-  role: string;
+  role: 'Admin' | 'ProjectManager' | 'Member';
   is_active: Generated<boolean>;
   created_at: Generated<Date>;
   updated_at: Generated<Date>;
@@ -40,7 +40,7 @@ export interface ProjectsTable {
   name: string;
   description: string | null;
   deadline: string | null;
-  status: string;
+  status: 'Active' | 'Archived';
   created_by: string;
   created_at: Generated<Date>;
   updated_at: Generated<Date>;
@@ -53,7 +53,7 @@ export interface TasksTable {
   assignee_id: string | null;
   title: string;
   description: string | null;
-  status: string;
+  status: 'ToDo' | 'InProgress' | 'Completed';
   priority: string | null;
   due_date: string | null;
   created_by: string;
